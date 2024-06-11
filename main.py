cb572ce9d9b94047cf54de50ef1ce5eaac50d7e1
loismontero <loismontero@gmail.com>
2024-06-11 16:42:35 -0500

    Update main.py
    otra opcion

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/hola")
+def read_root():
+    return {"Hola": "Maracaibo"}
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
